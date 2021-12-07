e8a0c083dfc96587242789cbdc9b1d4910adbd24
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-12-07 20:28:50 +0200

    Exercise 10.6: the sign in view
    sign in -view, first draft

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -7,15 +7,17 @@ import Theme from '../theme';
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: Theme.colors.appBar
+    backgroundColor: Theme.colors.appBar,
+    display: 'flex',
+    flexDirection:'row'
   },
-  // ...
 });
 
 const AppBar = () => {
   return (
         <View style={styles.container}>
-            <AppBarTab text={"repositories"} target={null} />
+            <AppBarTab text={"Repositories"} target={"/"} />
+            <AppBarTab text={"Sign in"} target={"/signin"} />
         </View>
     );
 };
--- rate-repository-app/src/components/AppBarTab.jsx
+++ rate-repository-app/src/components/AppBarTab.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Pressable, StyleSheet } from 'react-native';
+import { Link } from 'react-router-native';
 import Text from './Text';
 
 const styles = StyleSheet.create({
@@ -12,11 +13,12 @@ const styles = StyleSheet.create({
 });
 
 const AppBarTab = ({text, target}) => {
+    console.log("target", target);
     return (
       <Pressable>
-        <Text to={target} style={styles.container}>
+        <Link to={target} style={styles.container}>
             <Text style={styles.color} fontWeight="bold" fontSize="subheading">{text}</Text>
-        </Text>
+        </Link>
       </Pressable>
     );
   };
