b6194029fb8d1a4242746a1a6bb0ae3cce739b3b
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-12-08 20:37:04 +0200

    Exercise 10.15: storing the access token step2
    token is stored in context

--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -1,12 +1,20 @@
-import { useMutation } from '@apollo/client';
+import { useHistory } from "react-router-native";
+import { useMutation, useApolloClient } from '@apollo/client';
 import { LOGIN_USER } from '../graphql/mutations';
 
+import useAuthStorage from '../hooks/useAuthStorage';
+
 const useSignIn = () => {
- 
+    let history = useHistory();
+    const apolloClient = useApolloClient();
+    const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(LOGIN_USER);
 
   const signIn = async ({ username, password }) => {
     const result = await mutate({variables: {username, password}});
+    await authStorage.setAccessToken(result.data.authorize.accessToken);
+    apolloClient.resetStore();
+    history.push("/");
     return result;
   };
 
